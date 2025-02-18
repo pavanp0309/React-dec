1f7775b443c0384006939d1345a6a6a89292a414
pavanp0309 <pavanreddyp0309@gmail.com>
2025-02-18 11:54:32 +0530

    stop-watch

---
+++ useEffectprojects/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StopWatch from './components/stopwatch/StopWatch'
+
+const App = () => {
+  return (
+    <>
+      <StopWatch/>
+    </>
+  )
+}
+
+export default App
